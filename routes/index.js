5070fe500e71398eb70a5e52ce9dba655e98fc9a
Osia-me <olga.melnykovych@gmail.com>
2018-08-14 13:34:33 +0200

    admin role was done

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,13 @@ router.get("/register", function(req, res){
 //handle sign up logic
 router.post("/register", function(req, res){
   var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
-      req.flash("error", err.message);
-      return res.redirect("/register");
+  if(req.body.adminCode === 'secretcode123') {
+      newUser.isAdmin = true;
+    }
+    User.register(newUser, req.body.password, function(err, user){
+      if(err){
+        req.flash("error", err.message);
+        return res.redirect("/register");
     }
       passport.authenticate("local")(req, res, function(){
       req.flash("success", "Welcome to timeMe " + req.body.username);
